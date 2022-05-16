b44eb3c435ed8088cad49d51ae6cc843fd69e187
dynamichoos <43002332+dynamichoos@users.noreply.github.com>
2022-05-16 16:52:39 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -407,55 +407,6 @@ st.markdown('---')
 st.header(":palm_tree: Failure Classifier by analyzed results")
 st.markdown('####')
 
-################################################################################### test graphviz
-
-print('Here!!!')
-
-import streamlit as st
-import graphviz as graphviz
-
-# Create a graphlib graph object
-graph = graphviz.Digraph()
-graph.edge('run', 'intr')
-graph.edge('intr', 'runbl')
-graph.edge('runbl', 'run')
-graph.edge('run', 'kernel')
-graph.edge('kernel', 'zombie')
-graph.edge('kernel', 'sleep')
-graph.edge('kernel', 'runmem')
-graph.edge('sleep', 'swap')
-graph.edge('swap', 'runswap')
-graph.edge('runswap', 'new')
-graph.edge('runswap', 'runmem')
-graph.edge('new', 'runmem')
-graph.edge('sleep', 'runmem')
-
-st.graphviz_chart(graph)
-
-st.graphviz_chart('''
-    digraph {
-        run -> intr
-        intr -> runbl
-        runbl -> run
-        run -> kernel
-        kernel -> zombie
-        kernel -> sleep
-        kernel -> runmem
-        sleep -> swap
-        swap -> runswap
-        runswap -> new
-        runswap -> runmem
-        new -> runmem
-        sleep -> runmem
-    }
-''')
-
-
-###################################################################################
-
-
-
-
 print(df_selected)
 
 ###################################################################
@@ -477,11 +428,10 @@ os.environ["PATH"] += os.pathsep + dot_path
 
 ###################################################################
 
-
-
-
 viz=visualization_dtreeviz(df_selected,label_col)
-svg=viz.svg()
+#svg=viz.svg()
+
+st.image(viz._repr_svg_(), use_column_width=True)
 
 ###################################
 
